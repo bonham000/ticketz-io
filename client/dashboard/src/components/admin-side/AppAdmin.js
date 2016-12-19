cfa7b4b1634ccc848e1c1ac2ed418b5748e891e4
Sean Smith <sean.smith.2009@gmail.com>
2016-12-19 12:08:24 -0800

    fixed navbar toggle problem

--- client/dashboard/src/components/admin-side/AppAdmin.js
+++ client/dashboard/src/components/admin-side/AppAdmin.js
@@ -7,37 +7,43 @@ import $ from 'jquery';
 //to access the admins from any child component
 //this.props.admins[0].username
 class AppAdmin extends Component {
-    constructor(){
-      super();
-      this.state = {admins: [], username: <i className="fa fa-spinner fa-pulse" />, user: {}}
-    }
-    updateAdmins(data){
-			let updatedAdmins = this.state.admins;
-			let updatedUser = this.state.user;
-			
-    	if (data.task) {
-				updatedAdmins[data.index].task = data.task;
-    		updatedUser.task = data.task;
-			}
-			this.setState({admins: updatedAdmins, user: updatedUser})
+  constructor(){
+    super();
+    this.state = {
+    	admins: [],
+    	username: <i className="fa fa-spinner fa-pulse" />,
+    	user: {},
+    	showNav: true
+    };
+  }
+  updateAdmins(data){
+		let updatedAdmins = this.state.admins;
+		let updatedUser = this.state.user;
+		
+  	if (data.task) {
+			updatedAdmins[data.index].task = data.task;
+  		updatedUser.task = data.task;
 		}
-		createAdmin(data){
-    	let admins = this.state.admins;
-    	admins.push(data);
-			this.setState({admins: admins});
-		}
-		deleteAdmin(username){
-			let admins = this.state.admins;
-			let index = admins.map(function(admin){return admin.username}).indexOf(username);
-			admins.splice(index, 1)
-			this.setState({admins: admins});
-		}
-    handleLogout(){
-      $.get('/api/logout', function(res){
-        if (res){window.location.href = '/'}
-      })
-    }
-	componentWillMount(){
+		this.setState({admins: updatedAdmins, user: updatedUser})
+	}
+	createAdmin(data){
+  	let admins = this.state.admins;
+  	admins.push(data);
+		this.setState({admins: admins});
+	}
+	deleteAdmin(username){
+		let admins = this.state.admins;
+		let index = admins.map(function(admin){return admin.username}).indexOf(username);
+		admins.splice(index, 1)
+		this.setState({admins: admins});
+	}
+  handleLogout(){
+    $.get('/api/logout', function(res){
+      if (res){window.location.href = '/'}
+    })
+  }
+	componentDidMount(){
+		window.addEventListener('resize', this.handleResize);
 		$('body').css('background', 'none');
 		$('#admin-portal').hide();
 		$.get('/api/validateAuth', (user)=>{
@@ -50,48 +56,57 @@ class AppAdmin extends Component {
 			else window.location.href = '/app/login';
 		})
 	}
-	animateNavbar(){
-		if (document.getElementById('admin-navbar').style.left == '-200px') {
-			document.getElementById('admin-navbar').style.left = '0'
-		} else {
-			document.getElementById('admin-navbar').style.left = '-200px'
-		}
+	componentWillUnmount(){
+		window.removeEventListener('resize', this.handleResize);
 	}
-    render(){
-        return (
-            <div id="admin-portal">
-							
-							<div id="profile-box">
-								<div id="menu-btn-box" className="prof-box-btn" onClick={this.animateNavbar}><i className="fa fa-bars" /></div>
-								<div id="profile-box-name"><Link to="/admin">{this.state.username}</Link></div>
-								<div className="prof-box-btn"><Link to="/dashboard/settings"><i className="fa fa-cogs" /></Link></div>
-								<div className="prof-box-btn" onClick={()=>this.handleLogout()}><i className="fa fa-lock" /></div>
-							</div>
-							
-							
-                <div id="admin-navbar">
-									<Link to="/dashboard"><div className="navbtn">Dashboard</div></Link>
-									<Link to="/dashboard/tickets"><div className="navbtn">Tickets</div></Link>
-									<Link to="/dashboard/editusers"><div className="navbtn">Users</div></Link>
-									<Link to="/dashboard/tasks"><div className="navbtn">Tasks</div></Link>
-                </div>
-							
-                    <div id="admin-content">
-											{this.props.children &&
-												React.cloneElement(
-													this.props.children,
-													{admins: this.state.admins,
-														user: this.state.user,
-														username: this.state.username,
-														updateAdmins: (data)=>this.updateAdmins(data),
-														createAdmin: (data)=>this.createAdmin(data),
-														deleteAdmin: (data)=>this.deleteAdmin(data)
-													}
-												)}
-                    </div>
-            </div>
-        )
-    }
+	handleResize = () => {
+  	if (!this.state.showNav && window.innerWidth >= 768) {
+			document.getElementById('admin-navbar').style.left = '0';
+			this.setState({ showNav: true });
+  	} else if (this.state.showNav && window.innerWidth <= 767) {
+  		document.getElementById('admin-navbar').style.left = '-200px';
+  		this.setState({ showNav: false });
+  	};
+  }
+	animateNavbar(){
+		let elem = document.getElementById('admin-navbar');
+		(elem.style.left == '-200px') ? elem.style.left = '0' : elem.style.left = '-200px';
+	}
+  render(){
+    return (
+      <div id="admin-portal">
+				
+				<div id="profile-box">
+					<div id="menu-btn-box" className="prof-box-btn" onClick={this.animateNavbar}><i className="fa fa-bars" /></div>
+					<div id="profile-box-name"><Link to="/admin">{this.state.username}</Link></div>
+					<div className="prof-box-btn"><Link to="/dashboard/settings"><i className="fa fa-cogs" /></Link></div>
+					<div className="prof-box-btn" onClick={()=>this.handleLogout()}><i className="fa fa-lock" /></div>
+				</div>
+				
+        <div id="admin-navbar">
+					<Link to="/dashboard"><div className="navbtn">Dashboard</div></Link>
+					<Link to="/dashboard/tickets"><div className="navbtn">Tickets</div></Link>
+					<Link to="/dashboard/editusers"><div className="navbtn">Users</div></Link>
+					<Link to="/dashboard/tasks"><div className="navbtn">Tasks</div></Link>
+        </div>
+				
+        <div id="admin-content">
+					{this.props.children &&
+						React.cloneElement(
+							this.props.children,
+							{admins: this.state.admins,
+								user: this.state.user,
+								username: this.state.username,
+								updateAdmins: (data)=>this.updateAdmins(data),
+								createAdmin: (data)=>this.createAdmin(data),
+								deleteAdmin: (data)=>this.deleteAdmin(data)
+							}
+						)}
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default AppAdmin;
